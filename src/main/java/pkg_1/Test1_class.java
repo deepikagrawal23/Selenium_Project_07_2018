139b16156a26a869b20e887b25c13e11db1f791f
deepikagrawal23 <deepikagrawal23@gmail.com>
2018-07-22 16:14:20 +0530

    My second commit

--- src/main/java/pkg_1/Test1_class.java
+++ src/main/java/pkg_1/Test1_class.java
@@ -3,7 +3,8 @@ package pkg_1;
 public class Test1_class {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Testing GIT print code");
 
 	}
 
---
+++ src/main/java/pkg_1/Test2_class.java
@@ -0,0 +1,10 @@
+package pkg_1;
+
+public class Test2_class {
+
+	public static void main(String[] args) {
+		System.out.println("file 2 which is new");
+
+	}
+
+}
